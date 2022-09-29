ca181101c31e1a4da4711ffb86b107db6b930406
Andrea Zucchelli <zukka77@gmail.com>
2022-09-29 14:14:44 +0200

    feat: first commit

---
+++ mongo/mongo-init.js
@@ -0,0 +1,6 @@
+db = db.getSiblingDB("admin");
+db.createUser({user:"mongoadmin",pwd:"secret",roles:["userAdminAnyDatabase","dbAdminAnyDatabase","readWriteAnyDatabase"]})
+db = db.getSiblingDB("fse");
+db.createUser({user:"mongoadmin",pwd:"secret",roles:[{role:"readWrite",db:"fse"},{role:"read",db:"fse"}],mechanisms:["SCRAM-SHA-1","SCRAM-SHA-256"]})
+db = db.getSiblingDB("eds");
+db.createUser({user:"mongoadmin",pwd:"secret",roles:[{role:"readWrite",db:"eds"},{role:"read",db:"eds"}],mechanisms:["SCRAM-SHA-1","SCRAM-SHA-256"]})
