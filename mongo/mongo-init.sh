f066e28c2641031da2ebaf381c45a0063ac08a26
Andrea Prosseda <andreaprosseda@gmail.com>
2022-11-30 22:18:52 +0100

    feat: publication flow added (transformation only)

--- mongo/mongo-init.sh
+++ mongo/mongo-init.sh
@@ -8,5 +8,6 @@ mongoimport --db "fse" --collection schema --file "$collections_folder/schema.js
 mongoimport --db "fse" --collection schematron --file "$collections_folder/schematron.json" --jsonArray
 mongoimport --db "fse" --collection terminology --file "$collections_folder/terminology.json" --jsonArray
 mongoimport --db "fse" --collection dictionary --file "$collections_folder/dictionary.json" --jsonArray
+mongoimport --db "fse" --collection transform --file "$collections_folder/transform.json" --jsonArray
 
 echo "Done"
