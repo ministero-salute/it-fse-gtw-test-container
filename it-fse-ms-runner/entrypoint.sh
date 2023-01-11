f6b1b6153ece6bb9704330f61b698e6dae6d29f6
Andrea Prosseda <andreaprosseda@gmail.com>
2023-01-11 20:28:58 +0100

    feat(container): enhancements

--- it-fse-ms-runner/entrypoint.sh
+++ it-fse-ms-runner/entrypoint.sh
@@ -7,9 +7,9 @@ fi
 
 mkdir -p /app
 cd /app
-git clone --depth=1 --branch $BRANCH $URL .
+git clone --depth=1 --branch $BRANCH $URL . ||  { echo "fatal: failed to clone branch '$BRANCH' at url '$URL'" && exit 1; }
 git config pull.ff only
-git pull
+git pull ||  { echo "fatal: failed to pull branch '$BRANCH' at url '$URL'" && exit 1; }
 
 echo "running: mvn spring-boot:run -Dspring-boot.run.profiles=docker -Dspring-boot.run.arguments=\"--management.server.port=$PORT --server.port=$PORT\""
 exec mvn spring-boot:run -Dspring-boot.run.profiles=docker -Dspring-boot.run.arguments="--management.server.port=$PORT --server.port=$PORT"
