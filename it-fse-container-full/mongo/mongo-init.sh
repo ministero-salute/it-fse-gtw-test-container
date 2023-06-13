b73c58587aa0f5a0c0fd93f6a427f238047b9d6a
Vincenzo Ingenito <vincenzo.ingenito@hotmail.it>
2023-06-13 16:42:51 +0200

    feat: Container zstd

--- it-fse-container-full/mongo/mongo-init.sh
+++ it-fse-container-full/mongo/mongo-init.sh
@@ -2,7 +2,35 @@
 
 echo "Starting"
 
-collections_folder=var/lib/mongo/data
+zstd_folder=$(pwd)/docker-entrypoint-initdb.d/mongo-dump/input
+collections_folder=$(pwd)/docker-entrypoint-initdb.d/mongo-dump/output
+
+echo "Input folder: $zstd_folder"
+
+if [ ! -d "$collections_folder" ]; then
+    echo "Output folder: - $collections_folder - does not exist...it will be created"
+    mkdir $collections_folder || { echo "Failed to create folder $collections_folder"; exit 1; }
+else
+    echo "Collections folder already created..."
+fi
+
+echo "Decompressing files (.zstd)"
+
+# Enter the target folder containing .zstd
+cd $zstd_folder || { echo "Failed to enter folder ${zstd_folder}"; exit 1; }
+
+files=$(ls)
+
+# Iterate and extract
+for file in $files
+do
+  filename="${file%%.*}"
+  echo "Decompressing: $filename"
+  zstd -d < $file > $collections_folder/$filename.json || { echo "Failed to decompress $file"; exit 1; }
+done
+
+# Save on mongo
+echo "Importing collections on MongoDB"
 
 mongoimport --db "fse" --collection "schema" --file "$collections_folder/schema.json" --jsonArray
 mongoimport --db "fse" --collection "schematron" --file "$collections_folder/schematron.json" --jsonArray
@@ -14,5 +42,9 @@ mongoimport --db "fse" --collection "schema_eds" --file "$collections_folder/sch
 mongoimport --db "fse" --collection "schematron_eds" --file "$collections_folder/schematron.json" --jsonArray
 mongoimport --db "fse" --collection "terminology_eds" --file "$collections_folder/terminology.json" --jsonArray
 mongoimport --db "fse" --collection "transform_eds" --file "$collections_folder/transform.json" --jsonArray
+
+echo "Removing working folder"
+
+rm -rf $collections_folder || { echo "Failed to remove folder $collections_folder"; exit 1; }
 
 echo "Done"
