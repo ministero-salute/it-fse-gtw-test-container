4e3d63454ead7b58ef0f7049baa3ebb66bc72210
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2023-10-09 15:29:23 +0200

    Update mongo init script

--- it-fse-container-full/mongo/mongo-init.sh
+++ it-fse-container-full/mongo/mongo-init.sh
@@ -4,15 +4,15 @@ echo "Starting"
 
 collections_folder=var/lib/mongo/data
 
-mongoimport --db "fse" --collection "schema" --file "$collections_folder/schema.json" --jsonArray
-mongoimport --db "fse" --collection "schematron" --file "$collections_folder/schematron.json" --jsonArray
-mongoimport --db "fse" --collection "terminology" --file "$collections_folder/terminology.json" --jsonArray
-mongoimport --db "fse" --collection "transform" --file "$collections_folder/transform.json" --jsonArray
-mongoimport --db "fse" --collection "dictionary" --file "$collections_folder/dictionary.json" --jsonArray
+mongoimport --db "FSE_GTW" --collection "schema" --file "$collections_folder/schema.json" --jsonArray
+mongoimport --db "FSE_GTW" --collection "schematron" --file "$collections_folder/schematron.json" --jsonArray
+mongoimport --db "FSE_GTW" --collection "terminology" --file "$collections_folder/terminology.json" --jsonArray
+mongoimport --db "FSE_GTW" --collection "transform" --file "$collections_folder/transform.json" --jsonArray
+mongoimport --db "FSE_GTW" --collection "dictionary" --file "$collections_folder/dictionary.json" --jsonArray
 
-mongoimport --db "fse" --collection "schema_eds" --file "$collections_folder/schema.json" --jsonArray
-mongoimport --db "fse" --collection "schematron_eds" --file "$collections_folder/schematron.json" --jsonArray
-mongoimport --db "fse" --collection "terminology_eds" --file "$collections_folder/terminology.json" --jsonArray
-mongoimport --db "fse" --collection "transform_eds" --file "$collections_folder/transform.json" --jsonArray
+mongoimport --db "FSE_EDS" --collection "schema_eds" --file "$collections_folder/schema.json" --jsonArray
+mongoimport --db "FSE_EDS" --collection "schematron_eds" --file "$collections_folder/schematron.json" --jsonArray
+mongoimport --db "FSE_EDS" --collection "terminology_eds" --file "$collections_folder/terminology.json" --jsonArray
+mongoimport --db "FSE_EDS" --collection "transform_eds" --file "$collections_folder/transform.json" --jsonArray
 
 echo "Done"
--- it-fse-container-lite/mongo/mongo-init.sh
+++ it-fse-container-lite/mongo/mongo-init.sh
@@ -4,10 +4,10 @@ echo "Starting"
 
 collections_folder=var/lib/mongo/data
 
-mongoimport --db "fse" --collection "schema" --file "$collections_folder/schema.json" --jsonArray
-mongoimport --db "fse" --collection "schematron" --file "$collections_folder/schematron.json" --jsonArray
-mongoimport --db "fse" --collection "terminology" --file "$collections_folder/terminology.json" --jsonArray
-mongoimport --db "fse" --collection "transform" --file "$collections_folder/transform.json" --jsonArray
-mongoimport --db "fse" --collection "dictionary" --file "$collections_folder/dictionary.json" --jsonArray
+mongoimport --db "FSE_GTW" --collection "schema" --file "$collections_folder/schema.json" --jsonArray
+mongoimport --db "FSE_GTW" --collection "schematron" --file "$collections_folder/schematron.json" --jsonArray
+mongoimport --db "FSE_GTW" --collection "terminology" --file "$collections_folder/terminology.json" --jsonArray
+mongoimport --db "FSE_GTW" --collection "transform" --file "$collections_folder/transform.json" --jsonArray
+mongoimport --db "FSE_GTW" --collection "dictionary" --file "$collections_folder/dictionary.json" --jsonArray
 
 echo "Done"
