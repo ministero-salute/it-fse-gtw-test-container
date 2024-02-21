89f12eadcca98de8f5ad2b40468d5343582518e6
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2024-02-21 15:08:56 +0100

    fix: remove unused files

--- it-fse-gtw-container-full/mongo/mongo-init.js
+++
@@ -1,7 +0,0 @@
-db = db.getSiblingDB("admin");
-db.createUser({
-    user: "mongoadmin",
-    pwd: "secret",
-    roles: ["userAdminAnyDatabase", "dbAdminAnyDatabase", "readWriteAnyDatabase"],
-    mechanisms: [ "SCRAM-SHA-1","SCRAM-SHA-256" ]
-})
